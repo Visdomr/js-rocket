50d1ca7243ae6aa2aeb5285e02b010d61fbf91b0
Visdomr <runingback33@gmail.com>
2025-03-05 22:27:51 -0800

    fixed countdown priority in code so it starts on click, got rocket to launch successfully with a random number generator.

--- js/rocket.js
+++ js/rocket.js
@@ -4,11 +4,13 @@ var countdownNumber = 10;
 var changeState = function(state) {
 	document.body.className = 'body-state' + state;
 	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById('countdown').innerHTML = countdownNumber;
 
 	if (state == 2) {
 		timer = setInterval(function() {
-			document.getElementById('countdown').innerHTML = countdownNumber;
 			countdownNumber = countdownNumber - 1;
+			document.getElementById('countdown').innerHTML = countdownNumber;
 			if (countdownNumber <= 0) {
 				changeState(3);
 			};
@@ -17,7 +19,7 @@ var changeState = function(state) {
 	} else if (state == 3) {
 		var success = setTimeout(function() {
 			var randomNumber = Math.round(Math.random() * 10);
-                        console.log('randomNumber:', randomNumber)
+			console.log('randomNumber:', randomNumber)
 
 			if (randomNumber > 5) {
 				changeState(4);
